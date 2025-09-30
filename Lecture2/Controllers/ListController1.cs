83aa162d79d448852bab43181a168b30f15ccb5c
Your GitHub Username <giorgi.kazishvili@gau.edu.ge>
2025-09-30 20:36:36 +0400

    lecture 2

---
+++ Lecture2/Controllers/ListController1.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lecture2.Controllers
+{
+    public class ListController1 : Controller
+    {
+        // GET: ListController1
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ListController1/Details/5
+
+        [HttpGet("get-details/{category}/{page?}")]
+        public ActionResult Details(string category, int page = 1)
+        {
+            return View();
+        }
+
+        // GET: ListController1/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ListController1/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ListController1/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ListController1/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ListController1/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ListController1/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
